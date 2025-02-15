43f73c3ebf68543d4a471a4cdd03ca2b677ae0a8
Tanveer Singh <61462459+TanveerSingh527@users.noreply.github.com>
2025-02-15 16:03:23 -0600

    Added summary to the output

--- ai.py
+++ ai.py
@@ -1,9 +1,10 @@
 from google import genai
 
 title = ""
-content = ""
+content = '''
+'''
 
-prompt = f'''you are a bot that helps frustrated users determine whether or not an article is clickbait bullshit. an article is defined as bullshit if it promises something in the title but does not actually talk about it or beats around the bush. You will be given a title and the content of the article and you will determine whether or not it's bullshit in terms of bullshit percentage. You will first give Consensus %, then a breakdown, and then a final one line TLDR.
+prompt = f'''you are a bot that helps frustrated users determine whether or not an article is clickbait bullshit. an article is defined as bullshit if it promises something in the title but does not actually talk about it or beats around the bush. You will be given a title and the content of the article and you will determine whether or not it's bullshit in terms of bullshit percentage. You will first give Consensus %, then a summary of the article, and then a final one line TLDR.
 
 Example1)
 Title: Bloodborne is coming to Playstation 5
